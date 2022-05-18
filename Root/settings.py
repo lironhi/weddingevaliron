06b6d8d03587bb3f309e5b1818cc4c54132334c6
lironhi <lironbenharrouch@gmail.com>
2022-05-18 17:39:28 +0300

    Update settings.py

--- Root/settings.py
+++ Root/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'Root.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 #DATABASES = {
 #    'default': {
@@ -113,9 +113,9 @@ DATABASES = {
 #    }
 #}
 
-#DATABASES = {
-#    'default': dj_database_url.config()
-#}
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 
 
